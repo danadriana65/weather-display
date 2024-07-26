573b81eedfd415dec5a56d448c6764ae18fa35e3
danadriana65 <dan.adriana@cnmv.ro>
2024-07-26 10:57:32 +0300

    weather

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,10 +1,17 @@
 import "../styles/components/Forecast.css";
 
-export function Forecast() {
+export function Forecast({item}) {
   return (
-    <div className={`forecast`}>
-      <h1 className="title">Day's Forecast</h1>
-      <div className="forecast-list"></div>
+    <div className={"forecast-item"}>
+    <img
+     src={item.weatherCode.image}
+     alt={item.weatherCode.description}
+     className="forecast-icon"/>
+      <h1 className="temperature">{item.temperature}</h1>
+      <p className="time">
+        {item.hour}:
+      </p>
+      </div>
     </div>
   );
 }
